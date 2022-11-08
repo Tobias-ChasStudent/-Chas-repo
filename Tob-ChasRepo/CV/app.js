5f2853c0c73ff4bef9b59affe49c23f19e9f128b
Tobias <tobias.riiser@chasacademy.se>
2022-11-08 11:59:53 +0100

    Added modal event listener to "My projects" box

--- Tob-ChasRepo/CV/app.js
+++ Tob-ChasRepo/CV/app.js
@@ -16,12 +16,15 @@ function portfolioJSON() {
         //Loop through each one and create an element in divProjects
         arrPortfolio.forEach(element => {
             let itemKey = Object.keys(element)
+            let itemID = itemKey.toString()
+            itemID = itemID.replace(" ", "-")
+            console.log(itemID);
             //console.log(itemKey);
             //console.log(element[itemKey]);
             
             //Create the article box for the project
             let project = document.createElement("article")
-            project.id = itemKey
+            project.id = itemID
             
             //Create the title and append it to the article
             let header = document.createElement("h3")
@@ -36,6 +39,27 @@ function portfolioJSON() {
             //Append the project to divProjects
             divProjects.appendChild(project)
         });
+    })
+    .then(() => {
+        //Get the "My Projects" article
+        const articleMyProjects = document.querySelector("#My-Projects")
+
+        //Get the modal window
+        const sectionModal = document.querySelector(".modal")
+
+        //Get the modal content
+        const divModalContent = document.querySelector(".modal-content")
+
+        //Add event listener to the article to display a modal
+        articleMyProjects.addEventListener("click", () => {
+            sectionModal.classList.add("displayModal")
+        })
+
+        sectionModal.addEventListener("click", (e) => {
+            if (e.target != divModalContent) {
+                sectionModal.classList.remove("displayModal")
+            }
+        })
     })
 }
 portfolioJSON()
